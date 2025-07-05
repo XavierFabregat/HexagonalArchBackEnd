f0748327b9a6b98f4fc79d455b86cf2fe981fac6
XavierFabregat <xavi.fabregat.pous@gmail.com>
2025-07-05 21:38:37 +0200

    Add unit tests for GetAllTasksUseCase
    - Implemented tests for both successful and unsuccessful task retrieval scenarios.
    - Utilized mock classes to simulate task repository behavior for testing purposes.

---
+++ src/__tests__/application/use-cases/GetAllTasksUseCase.test.ts
@@ -0,0 +1,51 @@
+import { GetAllTasksUseCase } from '@application/use-cases/GetAllTasksUseCase';
+import { MockTaskRepository } from '../mocks/MockTaskRepository';
+import { TaskId } from '../../../domain/value-objects/TaskId';
+import { Task } from '../../../domain/entities/Task';
+import { MockIdGenerator } from '../mocks/MockIdGenerator';
+
+describe('GetAllTasksUseCase', () => {
+  let useCase: GetAllTasksUseCase;
+  let mockRepository: MockTaskRepository;
+  let mockIdGenerator: MockIdGenerator;
+
+  beforeEach(() => {
+    mockRepository = new MockTaskRepository();
+    useCase = new GetAllTasksUseCase(mockRepository);
+    mockIdGenerator = new MockIdGenerator();
+  });
+
+  afterEach(() => {
+    mockRepository.clear();
+  });
+
+  describe('Unsuccessful Task Retrieval', () => {
+    it('should return an empty array if there are no tasks', async () => {
+      const tasks = await useCase.execute();
+      expect(tasks).toBeDefined();
+      expect(tasks.length).toBe(0);
+    });
+  });
+
+  describe('Successful Task Retrieval', () => {
+    it('should return all tasks', async () => {
+      mockRepository.save(
+        Task.create(
+          'Test Task',
+          'Test Description',
+          TaskId.from(mockIdGenerator.generate())
+        )
+      );
+      mockRepository.save(
+        Task.create(
+          'Test Task 2',
+          'Test Description 2',
+          TaskId.from(mockIdGenerator.generate())
+        )
+      );
+      const tasks = await useCase.execute();
+      expect(tasks).toBeDefined();
+      expect(tasks.length).toBe(mockRepository.getTasks().length);
+    });
+  });
+});
